d55807a6fef4392b63a74ba1817d70051fe83b14
Jaivrat12 <jaivratdas123456@gmail.com>
2020-07-03 22:19:33 +0530

    Updates

---
+++ precache-manifest.00c3b00fd3de0842535950eb75dd9faa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a964893f272d2ef6475b32b6255f2f17",
+    "url": "/contact-manager-reactjs/index.html"
+  },
+  {
+    "revision": "e84dd310eba787d32868",
+    "url": "/contact-manager-reactjs/static/css/main.6e5b1b68.chunk.css"
+  },
+  {
+    "revision": "1ed353874f75c8c26cde",
+    "url": "/contact-manager-reactjs/static/js/2.093016af.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/contact-manager-reactjs/static/js/2.093016af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e84dd310eba787d32868",
+    "url": "/contact-manager-reactjs/static/js/main.5ce137da.chunk.js"
+  },
+  {
+    "revision": "9f1462dcca6a6bb50587",
+    "url": "/contact-manager-reactjs/static/js/runtime-main.34131b22.js"
+  }
+]);
